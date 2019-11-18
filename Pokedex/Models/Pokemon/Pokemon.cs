1d2584022a3375b22f1686cf072f4d4f342dd9ef
Barry Russell <brussell@FORTERACU.COM>
2019-11-18 16:58:39 -0600

    Now reading response as Pokemon model

--- Pokedex/Models/Pokemon/Pokemon.cs
+++ Pokedex/Models/Pokemon/Pokemon.cs
@@ -8,6 +8,9 @@ namespace Pokedex.Models.Pokemon
   public class Pokemon
   {
     public int id { get; set; }
-
+    public string name { get; set; }
+    public int base_experience { get; set; }
+    public int height { get; set; }
+    public int weight { get; set; }
   }
 }
