21a21b17282bdd7b1703154eb97072607459a0a1
Barry Russell <brussell@FORTERACU.COM>
2019-11-18 15:57:24 -0600

    WIP

---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Data.Dex;
+
+namespace Pokedex.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly Dex _dex;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async void Index(string Name)
+        {
+            Dex dex = new Dex();
+            Name = Name.ToLower();
+            await dex.GetPokeNumAsync(Name);
+            Console.WriteLine("Gotcha");
+        }
+    }
+}
